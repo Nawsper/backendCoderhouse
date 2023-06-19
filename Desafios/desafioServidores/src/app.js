3755d9c84045777c7a6d2678a668551a5b12214d
Yasmin Burrut <nawsper@gmail.com>
2023-06-19 16:32:59 -0300

    Se modifica ruta products

--- Desafios/desafioServidores/src/app.js
+++ Desafios/desafioServidores/src/app.js
@@ -12,8 +12,14 @@ const productManager = new ProductManager("./products.json");
 
 app.get("/products", async (req, res) => {
   try {
+    const limit = req.query.limit;
     const products = await productManager.readProducts();
-    res.status(200).json(products);
+    if (limit) {
+      const limitedProducts = products.slice(0, parseInt(limit));
+      res.status(200).json(limitedProducts);
+    } else {
+      res.status(200).json(products);
+    }
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -27,22 +33,6 @@ app.get("/products/:idProd", async (req, res) => {
       res.json(prod);
     } else {
       res.status(400).json({ message: "Product not found" });
-    }
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-});
-
-app.get("/products", async (req, res) => {
-  try {
-    const limit = req.query.limit;
-    const products = await productManager.readProducts();
-
-    if (limit) {
-      const limitedProducts = products.slice(0, parseInt(limit));
-      res.status(200).json(limitedProducts);
-    } else {
-      res.status(200).json(products);
     }
   } catch (error) {
     res.status(404).json({ message: error.message });
